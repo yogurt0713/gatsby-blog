787f697d675406072e060b93e71f4ea70f6fcc88
yogurt0713 <yogurt0713@gmail.com>
2023-06-17 21:30:21 +0900

    initial commit

--- content/blog/my-second-post/index.md
+++ content/blog/my-second-post/index.md
@@ -1,6 +1,7 @@
 ---
 title: My Second Post!
 date: "2015-05-06T23:46:37.121Z"
+description: second post description
 ---
 
 Wow! I love blogging so much already.
--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,7 @@ module.exports = {
     },
   },
   plugins: [
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
